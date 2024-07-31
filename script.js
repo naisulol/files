f0d483be373d8021750290b54492908b3e048d94
naisulol <itzoofytt@gmail.com>
2024-07-31 15:19:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+jQuery('body').addClass('hidden');
+const mq = window.matchMedia("(max-device-width : 1024px)");
+
+jQuery(document).ready(function ($) {
+	$('body').removeClass('hidden');
+	$('.carousel').carousel();
+
+
+	new WOW().init();
+
+	if (mq.matches) {
+		$('.wow').each(function () {
+			$(this).attr('data-wow-delay', '0s');
+		});
+	}
+});
+
